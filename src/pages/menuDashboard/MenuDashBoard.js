90ede3e6aab191f101bb8255327d2b691780d6a5
AramWondergem <aramwondergem@gmail.com>
2023-02-02 17:43:05 +0100

    started with the menudashboard

--- src/pages/menuDashboard/MenuDashBoard.js
+++ src/pages/menuDashboard/MenuDashBoard.js
@@ -1,15 +1,58 @@
 import React from 'react';
 import './menuDashboard.css';
 import Header from "../../components/header/Header";
+import Tile from "../../components/tile/Tile";
+import EmptyInputField from "../../components/inputWithLabel/emptyInputField/EmptyInputField";
+import Button from "../../components/button/Button";
+import InputCustomerDashboard from "../../components/inputWithLabel/inputCustomer/InputCustomerDashboard";
 
 function MenuDashBoard(props) {
     return (
         <>
             <Header/>
             <main className="menuDashboard outerbox">
-                <div className="menuDashboard--innerbox innerbox">
+                <form onSubmit={() => console.log("dit ga ik nog maken")}
+                    className="menuDashboard--innerbox innerbox flex-row">
+                    <Tile
+                        className="menuDashboard--orderstile"
+                    type="text">
+                        <div className="menuDashboard--waitinglist menuDashboard--orderstile-component">
+                            <h2>Waitinglist:</h2>
+                            <EmptyInputField>
+                                <InputCustomerDashboard/>
+                                <InputCustomerDashboard/>
+                                <InputCustomerDashboard/>
+                                <InputCustomerDashboard/>
+                            </EmptyInputField>
+                            <div>
+                                <Button
+                                    type="submit">
+                                    Decline
+                                </Button>
+                                <Button
+                                    type="submit">
+                                    Accept
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="menuDashboard--declinedOrders menuDashboard--orderstile-component">
+                            <h2>Declined orders:</h2>
+                            <EmptyInputField>
+                                <InputCustomerDashboard/>
+                                <InputCustomerDashboard/>
+                            </EmptyInputField>
+                            <div>
 
-                </div>
+                                <Button
+                                    type="submit">
+                                    Accept
+                                </Button>
+                            </div>
+
+                        </div>
+                    </Tile>
+
+                </form>
             </main>
         </>
     );
